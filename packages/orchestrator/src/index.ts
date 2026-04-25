36e651c22e5ef94676fe433b80cf8de28e27cf23
karan <naiduc332@gmail.com>
2026-04-25 17:57:05 +0530

    chore(orchestrator): export registry helpers from barrel

--- packages/orchestrator/src/index.ts
+++ packages/orchestrator/src/index.ts
@@ -1,1 +1,3 @@
 export type { OrchestratorOptions, RegistrySnapshot, RegistryAgent, SearchPlan, RankedResult, OrderRecord } from './types.js';
+export { loadRegistry, filterAgents, DEFAULT_REGISTRY_URL } from './registry.js';
+export type { AgentFilter, LoadRegistryInput } from './registry.js';
